d80a7c54e8dc51a913a7abf523b4bb9de253faf3
Ivan Gonzalez <ivan14polanco@gmail.com>
2018-09-10 08:42:56 -0400

    Add Masonry to Products component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Masonry from 'react-masonry-component';
 
 class Products extends Component {
   constructor(props) {
@@ -19,19 +20,14 @@ class Products extends Component {
   render() {
     //console.log(this.state.products)
     return (
-      <div className="products">
-	<div className="tile is-ancestor">
-	  <div className=" tile is-parent">
-	    <Product info={ {name: 'sdasdf', price:'1000'} } />
-	  </div>
-	  <div className=" tile is-parent">
-	    <Product info={ {name: 'sdasdf', price:'1000'} } />
-	  </div>
-	  <div className=" tile is-parent">
-	    <Product info={ {name: 'sdasdf', price:'1000'} } />
-	  </div>
-	</div>
-      </div>
+      <Masonry className={'columns'} >
+	{this.state.products.map(product =>
+	  { return <Product
+	    key={product.id}
+	    info={product} />
+	  }
+	)}
+      </Masonry>
     );
   }
 }
@@ -43,32 +39,34 @@ class Product extends Component {
       availability = <p className="not-available">no disponible</p>
     }
     return (
-      <article className="card">
-	<div className="card-image">
-	  <figure className="image is-4by3">
-	    <img src="https://bulma.io/images/placeholders/640x480.png"
-	      alt="Placeholder" />
-	  </figure>
-	</div>
-	<div className="card-content">
-	  <div className="media">
-	    <div className="media-content">
-	      <p className="title is-5">{this.props.info.name}</p>
-	      <p className="subtitle is-5">{this.props.info.price}</p>
+      <div className="column is-one-third">
+	<article className="card">
+	  <div className="card-image">
+	    <figure className="image is-4by3">
+	      <img src="https://bulma.io/images/placeholders/640x480.png"
+		alt="Placeholder" />
+	    </figure>
+	  </div>
+	  <div className="card-content">
+	    <div className="media">
+	      <div className="media-content">
+		<p className="title is-5">{this.props.info.name}</p>
+		<p className="subtitle is-5">{this.props.info.price}</p>
+	      </div>
+	    </div>
+	    <div className="content">
+	      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	      Phasellus nec iaculis mauris.
+	      {availability}
 	    </div>
 	  </div>
-	  <div className="content">
-	    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-	    nec iaculis mauris.
-	    {availability}
-	  </div>
-	</div>
-	<footer className="card-footer">
-	  <a href="" className="card-footer-item">Save</a>
-	  <a href="" className="card-footer-item">Edit</a>
-	  <a href="" className="card-footer-item">Delete</a>
-	</footer>
-      </article>
+	  <footer className="card-footer">
+	    <a href="" className="card-footer-item">Save</a>
+	    <a href="" className="card-footer-item">Edit</a>
+	    <a href="" className="card-footer-item">Delete</a>
+	  </footer>
+	</article>
+      </div>
     );
   }
 }
